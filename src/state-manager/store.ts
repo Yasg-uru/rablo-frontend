c9ebd18248d2a114b47b586bdc03d9922436b0ac
yasg-uru <yashpawar12122004@gmail.com>
2024-10-01 01:06:33 +0530

    setup of redux toolkit

---
+++ src/state-manager/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "product"],
+};
+
+const rootReducer = combineReducers({});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
