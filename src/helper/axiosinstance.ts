477ad961c3afa8ee0de77e5a1e4d20bd4e519bba
yasg-uru <yashpawar12122004@gmail.com>
2024-10-01 15:06:46 +0530

    configured deployed backend url in axiosinstance

--- src/helper/axiosinstance.ts
+++ src/helper/axiosinstance.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
-const axiosInstance = axios.create({ baseURL: "http://localhost:8000" });
+const axiosInstance = axios.create({ baseURL: "https://rablo-backend.vercel.app" });
 export default axiosInstance;
