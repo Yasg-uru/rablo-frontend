49347a0db642d1d15dd6566aeaef51a61fea08f1
yasg-uru <yashpawar12122004@gmail.com>
2024-10-01 16:56:02 +0530

    removed unused things

--- src/pages/product/createproduct.tsx
+++ src/pages/product/createproduct.tsx
@@ -1,6 +1,5 @@
-"use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -34,7 +33,7 @@ const formSchema = z.object({
 export default function CreateProductPage() {
   const dispatch = useAppDispatch();
   const { isLoading } = useAppSelector((state) => state.product);
-  const [imagePreview, setImagePreview] = useState<string | null>(null);
+//   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const { toast } = useToast();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
